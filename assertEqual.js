fb6495cae8583094ef74fef871ac6bb4a5238cfe
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-19 20:27:30 -0700

    Used assertEqual to compare data

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍🏻Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🥶Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("car", "dog");
