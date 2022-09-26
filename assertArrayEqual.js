89d81bb264797bfd4231674ed4723fe06da9a052
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-26 16:40:20 -0700

    Removed eqArrays and test code, added eqArrays 'require' logic and exported assertArraysEqual function

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function (arrayOne, arrayTwo) { 
-  if (arrayOne.length != arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  } 
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
@@ -20,9 +10,5 @@ const assertArraysEqual = function (arrayOne, arrayTwo) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // false 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // false
-
+module.exports = assertArraysEqual;
  
