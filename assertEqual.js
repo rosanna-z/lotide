6f6de8c8ecc5d051d1a689e6dd4862e9c08dc872
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-26 15:39:52 -0700

    Removed test code, exported the function

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍🏻Assertion Passed: " + actual + " === " + expected);
+    console.log(`👍🏻Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🥶Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🥶Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("car", "dog");
+
+
+module.exports = assertEqual;
