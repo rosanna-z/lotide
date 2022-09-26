268d19f847624faf97033bf04322a1e24bced00e
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-26 16:38:10 -0700

    Removed assertEqual function

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍🏻Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🥶Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // The tail is meant to be every element except the head (first element) of the array.
 const tail = function(array) {
   return array.slice(1);
