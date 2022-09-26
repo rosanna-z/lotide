defeaa2736fc878fd43f7c236989f3caefff99e2
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-26 16:02:13 -0700

    Removed test code and exported the function

--- tail.js
+++ tail.js
@@ -6,22 +6,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// The tail is meant to be every element except the head (first element) of the array.
 const tail = function(array) {
   return array.slice(1);
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case 2: ...
-const caseTwo = tail([23, 24, 25]);
-assertEqual(caseTwo[0], 24);
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
