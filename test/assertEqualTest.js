1faedbcb38c84420c0e2ab7a501a0852aaaa2af2
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-26 15:45:27 -0700

    Test code for assertEqual and added 'require' logic

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); // fail
+assertEqual(1, 1); // pass
+assertEqual("car", "dog"); // fail
+
