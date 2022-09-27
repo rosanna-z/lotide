a09683322c36a03f009f7d5abb916935e2ba420c
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-26 18:33:26 -0700

    Refactored tailTest to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,29 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: ...
-const caseTwo = tail([23, 24, 25]);
-assertEqual(caseTwo[0], 24);
+const assert = require('chai').assert;
 
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], "Labs");
+  });
+
+  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(words.length, 3);
+  });
+  it("returns 24 for [23, 24, 25]", () => {
+    const caseTwo = tail([23, 24, 25]);
+    assert.strictEqual(caseTwo[0], 24);
+  });
+
+});
