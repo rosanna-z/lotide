7b30d0c6cc6ab8844cbd8da6b8433c56f7afe2e1
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-27 00:33:05 -0700

    Referenced Index instead of function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,20 @@
-const middle = require('../middle');
+const _ = require('../index');
 const assert = require('chai').assert;
 
 describe("#middle", () => {
   it("returns [] for [1, 2]", () => {
-    assert.deepEqual(middle([1, 2]), []);
+    assert.deepEqual(_.middle([1, 2]), []);
   });
   it("returns [2] for [1, 2, 3]", () => {
-    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(_.middle([1, 2, 3]), [2]);
   });
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    assert.deepEqual(_.middle([1, 2, 3, 4, 5]), [3]);
   });
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(_.middle([1, 2, 3, 4]), [2, 3]);
   });
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4] );
+    assert.deepEqual(_.middle([1, 2, 3, 4, 5, 6]), [3, 4] );
   });
 });
