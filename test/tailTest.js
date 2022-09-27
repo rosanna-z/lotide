9c7f525e5a728acf5520c65900eacd31d8c382e1
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-27 00:33:12 -0700

    Referenced Index instead of function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,28 +1,27 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const _ = require('../index');
 const assert = require('chai').assert;
 
 
 describe("#tail", () => {
   it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
-    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const result = _.tail(["Hello", "Lighthouse", "Labs"]);
     assert.strictEqual(result.length, 2);
   });
   it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const result = _.tail(["Hello", "Lighthouse", "Labs"]);
     assert.strictEqual(result[0], "Lighthouse");
   });
   it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const result = _.tail(["Hello", "Lighthouse", "Labs"]);
     assert.strictEqual(result[1], "Labs");
   });
 
-  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
-    const words = ["Yo Yo", "Lighthouse", "Labs"];
-    assert.strictEqual(words.length, 3);
+  it("returns 2 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = _.tail(["Yo Yo", "Lighthouse", "Labs"]);
+    assert.strictEqual(words.length, 2);
   });
   it("returns 24 for [23, 24, 25]", () => {
-    const caseTwo = tail([23, 24, 25]);
+    const caseTwo = _.tail([23, 24, 25]);
     assert.strictEqual(caseTwo[0], 24);
   });
 
