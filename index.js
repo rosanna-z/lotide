fab47731ca19185330ec71e71078627c0e1783aa
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-26 19:54:28 -0700

    List of functions that our Lotide library has

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+const lotide = {
+  head: require('./head'),
+  tail: require('./tail'),
+  middle: require('./middle'),
+  takeUntil: require('./takeUnitl'),
+  without: require('./without'),
+  map: require('./map'),
+  flatten: require('./flatten'),
+  letterPositions: require('./letterPositions'),
+  findKeys: require('./findKeys'),
+  findKeyByValue: require('./findKeyByValue'),
+  countOnly: require('./countOnly'),
+  countLetters: require('./countLetters'),
+  eqArrays: require('./eqArrays'),
+  eqObjects: require('./eqObjects'),
+  assertEqual: require('./assertEqual'),
+  assertArraysEqual: require('./assertArraysEqual'),
+  assertObjectsEqual: require("./assertObjectsEqual")
+}
+
+module.exports = lotide;
