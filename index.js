8517646fec91cdaf40929b60147b29e1755cd103
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-27 00:50:55 -0700

    List of all functions in an object to export

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const lotide = {
   head: require('./head'),
   tail: require('./tail'),
   middle: require('./middle'),
-  takeUntil: require('./takeUnitl'),
+  takeUntil: require('./takeUntil'),
   without: require('./without'),
   map: require('./map'),
-  flatten: require('./flatten'),
   letterPositions: require('./letterPositions'),
-  findKeys: require('./findKeys'),
+  findKey: require('./findKey'),
   findKeyByValue: require('./findKeyByValue'),
   countOnly: require('./countOnly'),
   countLetters: require('./countLetters'),
   eqArrays: require('./eqArrays'),
   eqObjects: require('./eqObjects'),
   assertEqual: require('./assertEqual'),
-  assertArraysEqual: require('./assertArraysEqual'),
+  assertArraysEqual: require('./assertArrayEqual'),
   assertObjectsEqual: require("./assertObjectsEqual")
 }
 
