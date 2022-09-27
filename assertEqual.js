d3265767230953a6bfcf7497b9cd15d7fac09be7
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-27 00:44:58 -0700

    Updated function to return true/false

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`👍🏻Assertion Passed: ${actual} === ${expected}`);
+    return true;
   } else {
     console.log(`🥶Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   }
 };
-
-
 
 module.exports = assertEqual;
