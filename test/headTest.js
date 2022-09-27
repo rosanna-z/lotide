c4a8901eb07916466afeddb15364586b69cb95aa
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-27 00:32:59 -0700

    Referenced Index instead of function

--- test/headTest.js
+++ test/headTest.js
@@ -1,20 +1,20 @@
-const head = require('../head');
+const _ = require('../index');
 const assert = require('chai').assert;
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
+    assert.strictEqual(_.head([1, 2, 3]), 1);
   });
 
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(_.head(['5']), '5'); 
   });
 
   it("returns '5' for [5, 6, 7]", () => {
-    assert.strictEqual(head([5,6,7]), 5); 
+    assert.strictEqual(_.head([5,6,7]), 5); 
   });
   
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+    assert.strictEqual(_.head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
   });
 });
