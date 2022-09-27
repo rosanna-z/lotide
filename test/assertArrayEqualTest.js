a5827174e39a01b350138f870d2a6b36204566c8
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-27 00:32:18 -0700

    Referenced Index instead of function

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -1,7 +1,18 @@
-const assertArraysEqual = require('../assertArrayEqual');
+const _ = require('../index');
+const assert = require('chai').assert;
 
+describe("#assertArraysEqual", () => {
+  it("returns 'Assertion Passed' for [1, 2], [1, 2]", () => {
+    assert.strictEqual(_.assertArraysEqual([1, 2], [1, 2]), true);
+  });
+  it("returns 'Assertion Failed' for [1, 2], [3, 2]", () => {
+    assert.strictEqual(_.assertArraysEqual([1, 2], [3, 2]), false);
+  });
+    it("returns 'Assertion Passed' for ['1', '2'], ['1', '2']", () => {
+    assert.strictEqual(_.assertArraysEqual(['1', '2'], ['1', '2']), true);
+  });
+    it("returns 'Assertion Failed for ['1', '2', '3'], ['1', '2', 3]'", () => {
+    assert.strictEqual(_.assertArraysEqual(['1', '2', '3'], ['1', '2', 3]), false);
+  });
+});
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // false 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // false
